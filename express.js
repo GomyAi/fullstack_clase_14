f10d0a3adde90ff1f4a047dae9a4119e9f2af6bf
yesica godinez <yesica.gmata@hotmail.com>
2019-06-07 10:55:53 -0500

    express

--- express.js
+++ express.js
@@ -1,9 +1,23 @@
 const express = require('express')
+//const body_parser = require('body-parser');
 const app = express()
 const port = 9000
 
+
 app.get('/', function (req, res) {
 res.send('Hello World!')
+})
+
+app.post('/', function (req, res){
+    
+    res.send('Contestando una petición por: Post')
+})
+
+app.put('/', function (req, res){
+    res.send('Contestando una petición por: put')
+})
+app.delete('/', function (req, res){
+    res.send('Contestando una petición por: delete')
 })
 
 app.listen(port, function () { 
